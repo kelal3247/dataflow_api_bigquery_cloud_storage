2b10f8b8778740fbdd09ef5e645a8caaffabf61f
Timotius Kelvin <57350140+kelal3247@users.noreply.github.com>
2023-06-16 09:27:18 +0700

    Add files via upload

--- taxi_bigquery_rawdata.py
+++ taxi_bigquery_rawdata.py
@@ -1,107 +1,76 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "66734ed4",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import argparse\n",
-    "import requests\n",
-    "import apache_beam as beam\n",
-    "from apache_beam.io.gcp.bigquery import WriteToBigQuery\n",
-    "from apache_beam.io.textio import WriteToText\n",
-    "from apache_beam.options.pipeline_options import PipelineOptions\n",
-    "\n",
-    "def get_geojson_data(url):\n",
-    "    response = requests.get(url)\n",
-    "    return response.json()\n",
-    "\n",
-    "# Define a DoFn to yield each feature from the GeoJSON data\n",
-    "class GeoJSONToFeature(beam.DoFn):\n",
-    "    def process(self, element):\n",
-    "        features = element.get('features', [])\n",
-    "        for feature in features:\n",
-    "            yield feature\n",
-    "\n",
-    "def run_pipeline(project, dataset, table, temp_location, region, url, output_csv_path, job_name):\n",
-    "    # Define and initialize pipeline options\n",
-    "    pipeline_options = PipelineOptions(\n",
-    "        runner='DataflowRunner',\n",
-    "        project=project,\n",
-    "        temp_location=temp_location,\n",
-    "        region=region,\n",
-    "        job_name=job_name\n",
-    "    )\n",
-    "\n",
-    "    # Create a Pipeline\n",
-    "    with beam.Pipeline(options=pipeline_options) as pipeline:\n",
-    "        # Read data from the GeoJSON URL\n",
-    "        geojson_data = (\n",
-    "            pipeline\n",
-    "            | 'Generate URL' >> beam.Create([url])\n",
-    "            | 'Generate GeoJSON' >> beam.Map(get_geojson_data)\n",
-    "        )\n",
-    "\n",
-    "        # Extract features from the GeoJSON data\n",
-    "        features = geojson_data | 'Extract Features' >> beam.ParDo(GeoJSONToFeature())\n",
-    "\n",
-    "        # Write the features to BigQuery table\n",
-    "        features | 'Write to BigQuery' >> WriteToBigQuery(\n",
-    "            table=f'{dataset}.{table}',\n",
-    "            schema='field1:STRING, field2:INTEGER, ...',  # Modify the schema as per your requirements\n",
-    "            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,\n",
-    "            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND  # Append to the table for streaming\n",
-    "        )\n",
-    "\n",
-    "        # Write the features to Google Cloud Storage in CSV format\n",
-    "        features | 'Write to CSV' >> WriteToText(\n",
-    "            file_path_prefix=output_csv_path,\n",
-    "            file_name_suffix='.csv',\n",
-    "            header='field1,field2,...',  # Modify the header as per your requirements\n",
-    "            shard_name_template=''\n",
-    "        )\n",
-    "\n",
-    "if __name__ == '__main__':\n",
-    "    parser = argparse.ArgumentParser(description='Apache Beam Pipeline')\n",
-    "    parser.add_argument('--project', default='your_project', help='GCP Project ID')\n",
-    "    parser.add_argument('--dataset', default='your_dataset', help='BigQuery Dataset')\n",
-    "    parser.add_argument('--table', default='your_table', help='BigQuery Table')\n",
-    "    parser.add_argument('--temp_location', default='gs://your_bucket/temp', help='GCS Temporary Location')\n",
-    "    parser.add_argument('--region', default='your_region', help='GCP Region')\n",
-    "    parser.add_argument('--url', default='https://api.data.gov.sg/v1/transport/taxi-availability', help='GeoJSON URL')\n",
-    "    parser.add_argument('--output_csv_path', default='gs://your_bucket/output', help='Output CSV path')\n",
-    "    parser.add_argument('--job_name', default='your_job_name', help='Job name for Apache Beam')\n",
-    "\n",
-    "    args = parser.parse_args()\n",
-    "\n",
-    "    run_pipeline(args.project, args.dataset, args.table, args.temp_location, args.region, args.url, args.output_csv_path, args.job_name)\n",
-    "\n",
-    "# python my_script.py --job_name my_beam_job\n",
-    "# use Dataflow google cloud scheduler to schedule this job on 1 minuteinter"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import argparse
+import requests
+import apache_beam as beam
+from apache_beam.io.gcp.bigquery import WriteToBigQuery
+from apache_beam.io.textio import WriteToText
+from apache_beam.options.pipeline_options import PipelineOptions
+
+def get_geojson_data(url):
+    response = requests.get(url)
+    return response.json()
+
+# Define a DoFn to yield each feature from the GeoJSON data
+class GeoJSONToFeature(beam.DoFn):
+    def process(self, element):
+        features = element.get('features', [])
+        for feature in features:
+            yield feature
+
+def run_pipeline(project, dataset, table, temp_location, region, url, output_csv_path, job_name):
+    # Define and initialize pipeline options
+    pipeline_options = PipelineOptions(
+        runner='DataflowRunner',
+        project=project,
+        temp_location=temp_location,
+        region=region,
+        job_name=job_name
+    )
+
+    # Create a Pipeline
+    with beam.Pipeline(options=pipeline_options) as pipeline:
+        # Read data from the GeoJSON URL
+        geojson_data = (
+            pipeline
+            | 'Generate URL' >> beam.Create([url])
+            | 'Generate GeoJSON' >> beam.Map(get_geojson_data)
+        )
+
+        # Extract features from the GeoJSON data
+        features = geojson_data | 'Extract Features' >> beam.ParDo(GeoJSONToFeature())
+
+        # Define the schema dynamically from the features
+        schema = features | 'Get Schema' >> beam.schema.GenerateSchema()
+
+        # Write the features to BigQuery table
+        features | 'Write to BigQuery' >> WriteToBigQuery(
+            table=f'{dataset}.{table}',
+            schema=schema,
+            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND  # Append to the table for streaming
+        )
+
+        # Write the features to Google Cloud Storage in CSV format
+        features | 'Write to CSV' >> WriteToText(
+            file_path_prefix=output_csv_path,
+            file_name_suffix='.csv',
+            header=True,
+            shard_name_template=''
+        )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Apache Beam Pipeline')
+    parser.add_argument('--project', default='your_project', help='GCP Project ID')
+    parser.add_argument('--dataset', default='your_dataset', help='BigQuery Dataset')
+    parser.add_argument('--table', default='your_table', help='BigQuery Table')
+    parser.add_argument('--temp_location', default='gs://your_bucket/temp', help='GCS Temporary Location')
+    parser.add_argument('--region', default='your_region', help='GCP Region')
+    parser.add_argument('--url', default='https://api.data.gov.sg/v1/transport/taxi-availability', help='GeoJSON URL')
+    parser.add_argument('--output_csv_path', default='gs://your_bucket/output', help='Output CSV path')
+    parser.add_argument('--job_name', default='your_job_name', help='Job name for Apache Beam')
+
+    args = parser.parse_args()
+
+    run_pipeline(args.project, args.dataset, args.table, args.temp_location, args.region, args.url, args.output_csv_path, args.job_name)
+
+# python my_script.py --job_name my_beam_job
+# use Dataflow google cloud scheduler to schedule this job on 1 minute interval
